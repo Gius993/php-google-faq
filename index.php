91a65082bbcbd6fc3ef015f83a426ffa044b8447
Gius993 <giuseppevidili9@gmail.com>
2022-07-25 15:31:47 +0200

    header-completato

--- index.php
+++ index.php
@@ -23,8 +23,52 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	
+<header>
+        <div class="logo">
+            LOGO
+            <span>Privacy e termini</span>
+        </div>
+        <div class="navbar">
+            <ul class="list-navbar">
+                <li>
+                    <a href="#">
+                        Introduzione
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Norme sulla privacy
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Termini di servizio
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Tecnologie
+                    </a>
+                </li>
+                <li class="selected">
+                    <a href="#">
+                        Domande frequenti
+                    </a>
+                </li>
+            </ul>
+            <span><a href="#">Account Google</a></span>
+        </div>
+    </header>
+
+	<main class="main-container">
+		<div class="faq">
+			<h2></h2>
+			<p></p>
+		</div>
+	</main>
+
 </body>
 </html>
