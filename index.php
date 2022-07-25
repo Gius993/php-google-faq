13c16795c0af78df3fda617548184d5a9fe30e60
Gius993 <giuseppevidili9@gmail.com>
2022-07-25 15:18:43 +0200

    struttura e array fatti

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+    $faq = [
+        [
+            'title' => 'Alessandro',
+            'text' => 'Rossi',           
+        ],
+		[
+            'title' => 'Alessandro',
+            'text' => 'Rossi',            
+        ],
+		[
+            'title' => 'Alessandro',
+            'text' => 'Rossi',            
+        ],
+    ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
